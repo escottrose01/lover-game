9fe1ef94db3425bdc5bf60cf11052fc50365099c
Evan Rose <escottrose01@gmail.com>
2021-01-04 23:16:21 -0500

    Create Game Stat system

---
+++ Lover Game/Assets/Scripts/GameStats.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStats : MonoBehaviour
+{
+    private static GameStats instance;
+    public static GameStats Instance { get { return instance; } }
+
+    public static readonly int numLevels = 3; // not including secret level
+    public static int CurLevel { get; private set; }
+    public static bool SecretLevelUnlocked { get; private set; }
+
+    static int[] smileCount;
+    static int[] heartCount;
+
+    int levelHeartCount;
+    int levelSmileCount;
+    int levelNum;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this) Destroy(gameObject);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            SceneManager.sceneUnloaded += OnSceneUnloaded;
+
+            smileCount = new int[numLevels];
+            heartCount = new int[numLevels];
+
+            ReadStatsFromSave();
+        }
+    }
+
+    public void IncrementHearts()
+    {
+        ++levelHeartCount;
+    }
+
+    public void IncrementSmiles()
+    {
+        ++levelSmileCount;
+    }
+
+    public void SaveCounts()
+    {
+        heartCount[levelNum] = Mathf.Max(heartCount[levelNum], levelHeartCount);
+        smileCount[levelNum] = Mathf.Max(smileCount[levelNum], levelSmileCount);
+    }
+
+    void ReadStatsFromSave()
+    {
+        if (PlayerPrefs.HasKey("SaveFlag"))
+        {
+            CurLevel = PlayerPrefs.GetInt("CurLevel");
+
+            for (int i = 0; i < numLevels; ++i)
+            {
+                heartCount[i] = PlayerPrefs.GetInt("Hearts_Level" + i.ToString());
+                smileCount[i] = PlayerPrefs.GetInt("Smiles_Level" + i.ToString());
+            }
+        }
+    }
+
+    public void SaveStats()
+    {
+        PlayerPrefs.SetString("SaveFlag", "Hi Khrea! I love you lots <3");
+        PlayerPrefs.SetInt("CurLevel", CurLevel);
+
+        for (int i = 0; i < numLevels; ++i)
+        {
+            PlayerPrefs.SetInt("Hearts_Level" + i.ToString(), heartCount[i]);
+            PlayerPrefs.SetInt("Smiles_Level" + i.ToString(), smileCount[i]);
+        }
+    }
+
+    public void ClearStats()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    private void OnSceneUnloaded(Scene scene)
+    {
+        SaveCounts();
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        levelHeartCount = 0;
+        levelSmileCount = 0;
+        levelNum = SceneManager.GetActiveScene().buildIndex - 1;
+
+        SaveStats();
+    }
+}
--- Lover Game/Assets/Scripts/LevelMenu.cs
+++ Lover Game/Assets/Scripts/LevelMenu.cs
@@ -8,19 +8,15 @@ public class LevelMenu : MonoBehaviour
 {
     public GameObject levelArea;
     public GameObject levelIcon;
-    public int numLevels;
+    public Sprite LevelClearSprite;
+    public Sprite LevelUnlockedSprite;
+    public Sprite LevelLockedSprite;
     public int numCols;
-
-    Rect panelDimensions;
-    Rect iconDimensions;
 
     private void Start()
     {
-        panelDimensions = GetComponent<RectTransform>().rect;
-        iconDimensions = levelIcon.GetComponent<RectTransform>().rect;
-
-        int numRows = numLevels / numCols;
-        if (numLevels % numRows != 0) ++numCols;
+        int numRows = GameStats.numLevels / numCols;
+        if (GameStats.numLevels % numRows != 0) ++numCols;
 
         VerticalLayoutGroup verticalLayout = levelArea.AddComponent<VerticalLayoutGroup>();
         verticalLayout.childAlignment = TextAnchor.MiddleCenter;
@@ -42,18 +38,36 @@ public class LevelMenu : MonoBehaviour
             for (int c = 0; c < numCols; ++c)
             {
                 int levelNum = r * numCols + c + 1;
-                if (levelNum <= numLevels)
+                if (levelNum <= GameStats.numLevels)
                 {
-                    GameObject button = Instantiate(levelIcon, curRow.transform);
-                    button.name = "LevelButton-" + levelNum.ToString();
-                    button.GetComponentInChildren<TMP_Text>().text = "Level " + levelNum.ToString();
-                    button.GetComponent<Button>().onClick.AddListener(() => SceneLoader.Instance.LoadScene(levelNum));
+                    GameObject obj = Instantiate(levelIcon, curRow.transform);
+                    obj.name = "LevelButton-" + levelNum.ToString();
+                    obj.GetComponentInChildren<TMP_Text>().text = "Level " + levelNum.ToString();
+
+                    Button button = obj.GetComponent<Button>();
+                    Image image = obj.GetComponent<Image>();
+
+                    button.onClick.AddListener(() => SceneLoader.Instance.LoadScene(levelNum));
+
+                    if (levelNum < GameStats.CurLevel) image.sprite = LevelClearSprite;
+                    else if (levelNum == GameStats.CurLevel)
+                    {
+                        image.sprite = LevelUnlockedSprite;
+
+                        ColorBlock colors = button.colors;
+                        colors.normalColor = 0.8f * Color.white;
+                        button.colors = colors;
+                    }
+                    else
+                    {
+                        image.sprite = LevelLockedSprite;
+                        button.interactable = false;
+                    }
                 }
             }
         }
 
-        bool superSecretBool = false;
-        if (superSecretBool)
+        if (GameStats.SecretLevelUnlocked)
         {
             GameObject secretRow = new GameObject
             {
@@ -68,9 +82,9 @@ public class LevelMenu : MonoBehaviour
             secretHorizontal.childAlignment = TextAnchor.MiddleCenter;
 
             GameObject button = Instantiate(levelIcon, secretRow.transform);
-            button.name = "LevelButton-" + (numLevels + 1).ToString();
-            button.GetComponentInChildren<TMP_Text>().text = "Level " + (numLevels + 1).ToString();
-            button.GetComponent<Button>().onClick.AddListener(() => SceneLoader.Instance.LoadScene(numLevels + 1));
+            button.name = "LevelButton-" + (GameStats.numLevels + 1).ToString();
+            button.GetComponentInChildren<TMP_Text>().text = "Level " + (GameStats.numLevels + 1).ToString();
+            button.GetComponent<Button>().onClick.AddListener(() => SceneLoader.Instance.LoadScene(GameStats.numLevels + 1));
         }
     }
 }
