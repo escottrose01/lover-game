e1a7f1464343a1a4f4ea8d804479e77fa1027abd
Evan Rose <escottrose01@gmail.com>
2021-06-06 00:04:12 -0400

    Add different platform types

---
+++ Lover Game/Assets/Scripts/Platformer/CirclePlatformController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CirclePlatformController : PlatformController
+{
+    public float speed;
+    public float start;
+    public float radius;
+    float t;
+    Vector3 center;
+
+    public override void Start()
+    {
+        base.Start();
+        center = transform.position - radius * new Vector3(Mathf.Cos(start * 2 * Mathf.PI), Mathf.Sin(start * 2 * Mathf.PI), 0f);
+        t = start;
+    }
+
+    protected override Vector3 CalculatePlatformMovement()
+    {
+        float circumference = 2 * Mathf.PI * radius;
+        t += Time.deltaTime * speed / circumference;
+        t %= 1f;
+
+        return center + radius * new Vector3(Mathf.Cos(t * 2 * Mathf.PI), Mathf.Sin(t * 2 * Mathf.PI), 0f) - transform.position;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Vector3 center = Application.isPlaying ? this.center : transform.position - radius * new Vector3(Mathf.Cos(start * 2 * Mathf.PI), Mathf.Sin(start * 2 * Mathf.PI), 0f);
+        Gizmos.DrawWireSphere(center, radius);
+    }
+}
---
+++ Lover Game/Assets/Scripts/Platformer/PathPlatformController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathPlatformController : PlatformController
+{
+    public Vector3[] localWaypoints;
+    Vector3[] globalWaypoints;
+
+    public float speed;
+    public bool cyclic;
+    public float waitTime;
+
+    int fromWaypointIndex;
+    float percentBetweenWaypoints;
+    float nextMoveTime;
+
+    public override void Start()
+    {
+        base.Start();
+        globalWaypoints = new Vector3[localWaypoints.Length];
+        for (int i = 0; i < localWaypoints.Length; ++i)
+        {
+            globalWaypoints[i] = localWaypoints[i] + transform.position;
+        }
+    }
+
+    protected override Vector3 CalculatePlatformMovement()
+    {
+        if (Time.time < nextMoveTime) return Vector3.zero;
+
+        fromWaypointIndex %= globalWaypoints.Length;
+        int toWaypointIndex = (fromWaypointIndex + 1) % globalWaypoints.Length;
+        float distanceBetweenWaypoints = Vector3.Distance(globalWaypoints[fromWaypointIndex], globalWaypoints[toWaypointIndex]);
+        percentBetweenWaypoints += (distanceBetweenWaypoints != 0) ?
+            Time.deltaTime * speed / distanceBetweenWaypoints :
+            1f;
+
+        Vector3 newPos = Vector3.Lerp(globalWaypoints[fromWaypointIndex], globalWaypoints[toWaypointIndex], Tween.GetEasedValue(percentBetweenWaypoints, Tween.Easing.InOutSine));
+
+        if (percentBetweenWaypoints >= 1f)
+        {
+            percentBetweenWaypoints = 0f;
+            ++fromWaypointIndex;
+
+            if (!cyclic)
+            {
+                if (fromWaypointIndex == globalWaypoints.Length - 1)
+                {
+                    fromWaypointIndex = 0;
+                    System.Array.Reverse(globalWaypoints);
+                }
+            }
+
+            nextMoveTime = Time.time + waitTime;
+        }
+        return newPos - transform.position;
+    }
+
+    void OnDrawGizmos()
+    {
+        if (localWaypoints != null)
+        {
+            Gizmos.color = Color.red;
+            float size = 0.3f;
+            int max = localWaypoints.Length;
+            if (globalWaypoints != null) max = Mathf.Min(max, globalWaypoints.Length);
+            for (int i = 0; i < max; ++i)
+            {
+                Vector3 globalWaypointPosition = (globalWaypoints != null) ? globalWaypoints[i] : localWaypoints[i] + transform.position;
+                Gizmos.DrawLine(globalWaypointPosition + Vector3.down * size, globalWaypointPosition + Vector3.up * size);
+                Gizmos.DrawLine(globalWaypointPosition + Vector3.left * size, globalWaypointPosition + Vector3.right * size);
+            }
+        }
+    }
+}
